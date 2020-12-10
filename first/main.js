267be876b1f6cc1e15dd5db7b242525e66035005
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2020-12-10 11:52:41 +0900

    mainの変更,数あての作成

--- first/main.js
+++ first/main.js
@@ -1,10 +1,13 @@
 let myHeading = document.querySelector('h1');
-const myButton = document.querySelector('button');
-myButton.onclick = () => {
+let gazou = document.querySelector('img');
+const myButton1 = document.getElementById('midasi');
+
+myButton1.onclick = () => {
     //alert("ボタンがクリックされました")
     if(myHeading.textContent === "webテクノロジーB 最初のサンプル！"){
-        myHeading.textContent = "hogehoge";
+        myHeading.textContent = "g19911 Kakizaki Hinano";
     }else{
         myHeading.textContent = "webテクノロジーB 最初のサンプル！";
     }
 }
+
